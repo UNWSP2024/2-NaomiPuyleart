0975985cbfa3db19a992d801fd95981349d993b6
NaomiPuyleart <nipuyleart@students.unwsp.edu>
2025-09-12 20:43:39 -0500

    Commit

--- program_1.py
+++ program_1.py
@@ -1,5 +1,12 @@
 def personal_information():
-    print('...')
+    #Name
+    print("Naomi")
+#Address: City, State, Zip
+    print("The Capital, Alabama, 33333")
+#Phone Number
+    print("123-456-7890")
+#(Future) Major
+    print("Cybersecurity Administration")
 
 
 # Line which calls the above function.
